c2b3e774800a59adc65770898309200506b05b6c
Xavier G <xgarcia3622@sdsu.edu>
2026-02-25 15:24:11 -0800

    Commit 1: setup project + implement Core A addSpace (circular, max 40)

---
+++ monopoly_board.cpp
@@ -0,0 +1,40 @@
+// File: monopoly_board.cpp
+// Created by axavi on 2/25/2026.
+
+#include "monopoly_board.h"
+
+// -------------------------------
+// MonopolySpace definitions
+// -------------------------------
+MonopolySpace::MonopolySpace() {
+    propertyName = "";
+    propertyColor = "";
+    value = 0;
+    rent = 0;
+}
+
+MonopolySpace::MonopolySpace(string propertyName, string propertyColor, int value, int rent) {
+    this->propertyName = propertyName;
+    this->propertyColor = propertyColor;
+    this->value = value;
+    this->rent = rent;
+}
+
+bool MonopolySpace::isEqual(MonopolySpace other) {
+    return propertyName == other.propertyName;
+}
+
+void MonopolySpace::print() {
+    cout << propertyName << " | " << propertyColor
+         << " | $" << value << " | Rent " << rent << endl;
+}
+
+int main() {
+    CircularLinkedList<MonopolySpace> board;
+
+    cout << (board.addSpace(MonopolySpace("GO", "None", 0, 0)) ? "Added GO" : "Failed GO") << endl;
+    cout << (board.addSpace(MonopolySpace("Mediterranean Avenue", "Brown", 60, 2)) ? "Added Med" : "Failed Med") << endl;
+
+    cout << "Board size: " << board.size() << endl;
+    return 0;
+}
---
+++ monopoly_board.h
@@ -0,0 +1,91 @@
+// File: monopoly_board.h
+// Created by axavi on 2/25/2026.
+
+
+#ifndef MONOPOLY_BOARD_MONOPOLY_BOARD_H
+#define MONOPOLY_BOARD_MONOPOLY_BOARD_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static const int MAX_SPACES = 40;
+
+// ------------------------------------
+// Data class
+// ------------------------------------
+
+class MonopolySpace {
+    public:
+    string propertyName;
+    string propertyColor;
+    int value;
+    int rent;
+
+    MonopolySpace();
+    MonopolySpace(string propertyName, string propertyColor, int value, int rent);
+
+    bool isEqual(MonopolySpace other);
+    void print();
+};
+
+// ------------------------------------
+// Template Node Class
+// ------------------------------------
+template <typename T>
+class Node {
+    public:
+    T data;
+    Node<T>* nextNode;
+
+    explicit Node(T value) : data(value), nextNode(nullptr) {}
+};
+
+// ---------------------------------------
+// Template Circular Linked List class
+// Spring 2026 version: traversable board
+// ---------------------------------------
+template <typename T>
+class CircularLinkedList {
+    private:
+        Node<T>* headNode;
+        Node<T>* tailNode;
+
+        Node<T>* playerNode;
+        int nodeCount;
+        int passGoCount;
+
+    public:
+        CircularLinkedList()
+            : headNode(nullptr),
+              tailNode(nullptr),
+              playerNode(nullptr),
+              nodeCount(0),
+              passGoCount(0) {}
+        // Core A: Add a Space with Capacity Enforcement
+        bool addSpace(T value) {
+                if (nodeCount >= MAX_SPACES) {
+                    return false; // reject cleanly without touching pointers
+                }
+
+                Node<T>* newNode = new Node<T>(value);
+
+                if (headNode == nullptr) {
+                    headNode = newNode;
+                    tailNode = newNode;
+                    playerNode = newNode;
+                    tailNode->nextNode = headNode; // start the ring
+                } else {
+                    tailNode->nextNode = newNode;
+                    tailNode = newNode;
+                    tailNode->nextNode = headNode; // keep ring intact
+                }
+            nodeCount++;
+            return true;
+            }
+    int size() const { return nodeCount; }
+
+};
+#endif //MONOPOLY_BOARD_MONOPOLY_BOARD_H
